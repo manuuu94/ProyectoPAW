93c51e945f9ddf28d53bd3440975cddda463baa3
manu_ <manu_@LAPTOP-7HTT7HMN>
2022-09-26 19:44:44 -0600

    UnidadTrabajo

---
+++ DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        //se instancia una conexion y alIDALGenerico que contiene los métodos genéricos.
+        private readonly PROYECTO_PAWContext context;
+        //public IDALGenerico<Queja> quejaDAL;
+        //public IDALGenerico<TablaGeneral> tablaDAL;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(PROYECTO_PAWContext _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);         
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                string msj = e.Message;
+                return false;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ DAL/Interfaces/IDALGenerico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    //convertir en clase publica, en lugar de internal.
+    //parametro de lista, para que reciba cualquier cosa. La clase generica puede ser de cualquier cosa.
+    //puede especificar con un where, que entity sea una clase, por ejemplo
+    public interface IDALGenerico<TEntity> where TEntity : class
+    {
+        //un GET
+        TEntity Get(int id);
+        //un GET ALL a una lista
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        // This method was not in the videos, but I thought it would be useful to add.
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        
+        //un ADD o Añadir
+        bool Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        //un UPDATE o Actualizar que reciben una clase TEntity, cualquier clase.
+        bool Update(TEntity entity);
+        //un REMOVE o Eliminar
+        bool Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+    }
+}
