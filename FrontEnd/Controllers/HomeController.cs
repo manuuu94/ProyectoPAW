396838698ad2c4169448dd379a53a4edee74e628
Luis Avila <lavila20658@ufide.ac.r>
2022-10-31 19:56:20 -0600

    Seccion Ubicacion

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FrontEnd.Controllers
             return View();
         }
 
+        public IActionResult Ubicacion()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
