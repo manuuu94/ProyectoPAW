fb0cf35d4965e8a607b017bc0ef67aaf0a696c95
Luis Avila <lavila20658@ufide.ac.r>
2022-10-31 19:36:26 -0600

    ClientesAtendidos

---
+++ FrontEnd/Controllers/ClientesAtendidoController.cs
@@ -0,0 +1,30 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Controllers
+{
+    public class ClientesAtendidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                ServiceRepository Repository = new ServiceRepository();
+                HttpResponseMessage responseMessage = Repository.GetResponse("api/ClientesAtendidos");
+                responseMessage.EnsureSuccessStatusCode();
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                List<ClientesAtendidoViewModel> clientesatendidos = JsonConvert.DeserializeObject<List<ClientesAtendidoViewModel>>(content); //lista
+
+                return View(clientesatendidos);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
+
+
---
+++ FrontEnd/Models/ClientesAtendidoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FrontEnd.Models
+{
+    public class ClientesAtendidoViewModel
+    {
+        public int IdCliente { get; set; }
+        public string CedulaCliente { get; set; } = null!;
+        public string? NombreCliente { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public DateTime? Fecha { get; set; }
+        public int? IdCompra { get; set; }
+    }
+}
