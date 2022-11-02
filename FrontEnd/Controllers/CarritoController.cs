9baff35c40c5883752374a69dc7a33c7a45c5807
Luis Avila <lavila20658@ufide.ac.r>
2022-11-01 21:43:12 -0600

    VerCarrito

---
+++ FrontEnd/Controllers/CarritoController.cs
@@ -0,0 +1,28 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Controllers
+{
+    public class CarritoController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                ServiceRepository Repository = new ServiceRepository();
+                HttpResponseMessage responseMessage = Repository.GetResponse("api/Carrito");
+                responseMessage.EnsureSuccessStatusCode();
+                var content = responseMessage.Content.ReadAsStringAsync().Result;
+                List<CarritoViewModel> carrito = JsonConvert.DeserializeObject<List<CarritoViewModel>>(content); //lista
+
+                return View(carrito);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ FrontEnd/Models/CarritoViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FrontEnd.Models
+{
+    public class CarritoViewModel
+    {
+        public int IdPro { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Cantidad { get; set; }   
+
+        public decimal Total { get; set; }  
+
+        public int IdProducto { get; set; }
+
+    }
+
+}
