7b5d0724fa7863339b66fb541eb7a3af7777093f
Luis Avila <lavila20658@ufide.ac.r>
2022-11-01 21:01:24 -0600

    RegistrarEmpleadoNuevo

---
+++ FrontEnd/Controllers/EmpleadoNuevoController.cs
@@ -0,0 +1,44 @@
+﻿using FrontEnd.Helpers;
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class EmpleadoNuevoController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmpleadoNuevoViewModel empleadoNuevo)
+        {
+            try
+            {
+
+
+
+                ServiceRepository serviceObj = new ServiceRepository();
+                HttpResponseMessage response = serviceObj.PostResponse("api/Empleado", empleadoNuevo);
+                response.EnsureSuccessStatusCode();
+                EmpleadoNuevoViewModel EmpleadoNuevoViewModel = response.Content.ReadAsAsync<EmpleadoNuevoViewModel>().Result;
+                return RedirectToAction("Index","Empleado");
+            }
+            catch (HttpRequestException
+          )
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            catch (Exception
+            )
+            {
+
+                throw;
+            }
+        }
+    }
+    }
---
+++ FrontEnd/Models/EmpleadoNuevoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FrontEnd.Models
+{
+    public class EmpleadoNuevoViewModel
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido1 { get; set; } = null!;
+        public string Apellido2 { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public int IdRol { get; set; }
+    }
+}
