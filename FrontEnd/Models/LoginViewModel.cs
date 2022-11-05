4da0ef42a4f6ebd1370912c152b4e9ef76deb5f1
manu_ <manu_@LAPTOP-7HTT7HMN>
2022-11-05 15:11:32 -0600

    tasks & login

--- FrontEnd/Models/LoginViewModel.cs
+++ FrontEnd/Models/LoginViewModel.cs
@@ -6,8 +6,8 @@ namespace FrontEnd.Models
     {
 
         [Required]
-        public string Username { get; set; } /*= null!;*/
+        public string Username { get; set; } = null!;
         [Required]
-        public string Password { get; set; } /*= null!;*/
+        public string Password { get; set; } = null!;
     }
 }
