a492fb51157b458de9f052573c5c7d23d436acce
manu_ <manu_@LAPTOP-7HTT7HMN>
2022-11-20 17:35:03 -0600

    js

---
+++ FrontEnd/wwwroot/js/Carrito.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#tbCarro').DataTable(
+        {
+            "bFilter": true,
+            "bInfo": true,
+            "bLengthChange": true,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'excel', 'pdf'
+            ]
+        }
+    );
+});
+
+
+function validar() {
+    var test = document.getElementById("id_prod");
+    if (test == null) {
+        MostrarAlerta("CARRITO VACIO", 'error');
+        return false;
+    }
+    return true;
+};
