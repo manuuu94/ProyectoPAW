591bf173f063307ae31e16e7a19237f75695c3f1
carloslalinde <54185636+carloslalinde@users.noreply.github.com>
2022-11-26 23:38:55 -0600

    Cambios display name carrito view model

--- FrontEnd/Models/CarritoViewModel.cs
+++ FrontEnd/Models/CarritoViewModel.cs
@@ -1,17 +1,25 @@
-﻿namespace FrontEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FrontEnd.Models
 {
     public class CarritoViewModel
     {
+        [Display(Name = "Id")]
         public int IdProd { get; set; }
 
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
 
+        [Display(Name = "Precio")]
         public decimal Precio { get; set; }
 
-        public int Cantidad { get; set; }   
+        [Display(Name = "Cantidad")]
+        public int Cantidad { get; set; }
 
-        public decimal Total { get; set; }  
-
+        [Display(Name = "Total")]
+        public decimal Total { get; set; }
+        [Display(Name = "Id Producto")]
         public int IdProducto { get; set; }
 
     }
