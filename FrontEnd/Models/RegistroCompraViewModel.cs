a0ecd36d424f6fe71fb77c5bf77c9df64bd9eff0
carloslalinde <54185636+carloslalinde@users.noreply.github.com>
2022-11-26 23:27:26 -0600

    Cambios a table head de Index registro compra

--- FrontEnd/Models/RegistroCompraViewModel.cs
+++ FrontEnd/Models/RegistroCompraViewModel.cs
@@ -1,20 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace FrontEnd.Models
 {
     public class RegistroCompraViewModel
     {
+        [Display(Name = "Id Compra")]
         public int IdCompra { get; set; }
+
+        [Display(Name = "Cedula cliente")]
         public string CedulaCliente { get; set; } = null!;
+
+        [Display(Name = "Nombre cliente")]
         public string? NombreCliente { get; set; }
+
+        [Display(Name = "Correo")]
         public string? Correo { get; set; }
+
+        [Display(Name = "Telefono")]
         public string? Telefono { get; set; }
+
+        [Display(Name = "Fecha")]
         public DateTime? Fecha { get; set; }
+
+        [Display(Name = "Total de compra")]
         public decimal? TotalCompra { get; set; }
+
+        [Display(Name = "Metodo de pago")]
         public int IdMetodo { get; set; }
+
+        [Display(Name = "Empleado")]
         public int IdEmpleado { get; set; }
         public string? NombreEmpleado { get; set; }
-        
+
         [NotMapped]
         public string? TotalCompraStr { get; set; }
 
